403ba005c92f9681014eb5d46295569ef1ad127a
Laszlo Tovari <laszlo.tovari@awaxo.com>
2024-05-22 23:46:18 +0200

    Increase version number

--- config/variables.php
+++ config/variables.php
@@ -3,7 +3,7 @@
   return [
     "creatorName" => "HUN-REN TTK",
     "creatorUrl" => "https://www.ttk.hun-ren.hu/",
-    "appVersion" => "0.6.0",
+    "appVersion" => "0.6.1",
 
     "templateName" => "Workflow",
     "templateSuffix" => "Bootstrap 5 HTML Admin Template - Pro",

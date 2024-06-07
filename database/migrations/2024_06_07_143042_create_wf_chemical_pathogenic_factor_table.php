8f773b3de53cf090324027fc062cc27614f30c2d
Laszlo Tovari <laszlo.tovari@awaxo.com>
2024-06-07 14:40:08 +0200

    Add checmical pathogenic material model and migration

---
+++ database/migrations/2024_06_07_143042_create_wf_chemical_pathogenic_factor_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up() {
+        Schema::create('wf_chemical_pathogenic_factor', function (Blueprint $table) {
+            $table->id();
+            $table->string('factor');
+            $table->tinyInteger('deleted')->unsigned()->default(0);
+            $table->timestamps();
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by');
+
+            $table->foreign('created_by')->references('id')->on('wf_user');
+            $table->foreign('updated_by')->references('id')->on('wf_user');
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('wf_chemical_pathogenic_factor');
+    }
+};

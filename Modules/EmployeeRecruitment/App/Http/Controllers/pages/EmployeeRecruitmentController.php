fcacb7bdab8120ed8790688b2b8ff273a7c7fdcb
Laszlo Tovari <laszlo.tovari@awaxo.com>
2024-06-22 15:34:20 +0200

    Salary section order on view page

--- Modules/EmployeeRecruitment/App/Http/Controllers/pages/EmployeeRecruitmentController.php
+++ Modules/EmployeeRecruitment/App/Http/Controllers/pages/EmployeeRecruitmentController.php
@@ -20,7 +20,6 @@ use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Modules\EmployeeRecruitment\App\Models\RecruitmentWorkflow;

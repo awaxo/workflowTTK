86b977a8ae157b7ad682a01a657f31cd712fa650
Laszlo Tovari <laszlo.tovari@awaxo.com>
2024-07-10 17:22:07 +0200

    Logo link removed Javascript error corrected

--- resources/views/layouts/sections/menu/verticalMenu.blade.php
+++ resources/views/layouts/sections/menu/verticalMenu.blade.php
@@ -7,12 +7,12 @@ $configData = Helper::appClasses();
   <!-- ! Hide app brand if navbar-full -->
   @if(!isset($navbarFull))
   <div class="app-brand demo">
-    <a href="{{url('/')}}" class="app-brand-link">
+    <!--<a href="{{url('/')}}" class="app-brand-link">-->
       <span class="app-brand-logo demo">
         @include('_partials.macros',["width"=>25,"withbg"=>'var(--bs-primary)'])
       </span>
       <span class="app-brand-text demo menu-text fw-bold ms-2">{{config('variables.templateName')}}</span>
-    </a>
+    <!--</a>-->
 
     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
       <i class="bx bx-chevron-left bx-sm align-middle"></i>

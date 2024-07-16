e87ed9b2ac4f1a0724a1e17edfa06d98581f1a6b
Laszlo Tovari <laszlo.tovari@awaxo.com>
2024-07-17 01:39:33 +0200

    Version number change

--- config/variables.php
+++ config/variables.php
@@ -3,7 +3,7 @@
   return [
     "creatorName" => "HUN-REN TTK",
     "creatorUrl" => "https://www.ttk.hun-ren.hu/",
-    "appVersion" => "0.23.0",
+    "appVersion" => "0.22.3",
 
     "templateName" => "Ügyintézés",
     "templateSuffix" => "Bootstrap 5 HTML Admin Template - Pro",

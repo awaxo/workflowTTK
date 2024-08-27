30decc60c7aeb828549bb8aea0749953835d8448
Laszlo Tovari <laszlo.tovari@awaxo.com>
2024-08-27 17:21:12 +0200

    Add recruitment pdf link to details page

--- Modules/EmployeeRecruitment/resources/views/content/_partials/all-approval.blade.php
+++ Modules/EmployeeRecruitment/resources/views/content/_partials/all-approval.blade.php
@@ -438,6 +438,14 @@
                 @endif
             </div>
             <div class="d-flex">
+                <label class="form-label col-6 col-md-3">Felvételi kérelem</label>
+                @if($recruitment->medical_eligibility_data)
+                    <span class="fw-bold ms-1 text-break"><a href="/generate-pdf/{{ $recruitment->id }}" target="_blank">felvételi kérelem megtekintése</a></span>
+                @else
+                    <span class="fw-bold ms-1 text-break">-</span>
+                @endif
+            </div>
+            <div class="d-flex">
                 <label class="form-label col-6 col-md-3">Beutaló munkaköri orvosi alkalmassági vizsgálatra</label>
                 @if($recruitment->medical_eligibility_data)
                     <span class="fw-bold ms-1 text-break"><a href="/generate-medical-pdf/{{ $recruitment->id }}" target="_blank">adatlap megtekintése</a></span>
--- config/variables.php
+++ config/variables.php
@@ -3,7 +3,7 @@
   return [
     "creatorName" => "HUN-REN TTK",
     "creatorUrl" => "https://www.ttk.hun-ren.hu/",
-    "appVersion" => "1.3.0",
+    "appVersion" => "1.4.0",
 
     "templateName" => "Ügyintézés",
     "templateSuffix" => "",

c507feb97bb10507b027c43d234d8a55dbf7aa0f
Laszlo Tovari <laszlo.tovari@awaxo.com>
2024-09-02 07:54:21 +0200

    Version number increase

--- config/variables.php
+++ config/variables.php
@@ -3,7 +3,7 @@
   return [
     "creatorName" => "HUN-REN TTK",
     "creatorUrl" => "https://www.ttk.hun-ren.hu/",
-    "appVersion" => "1.6.0",
+    "appVersion" => "1.6.1",
 
     "templateName" => "Ügyintézés",
     "templateSuffix" => "",
